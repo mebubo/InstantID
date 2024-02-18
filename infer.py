3a95a80d25226d0cdaf48b660533fb2b8057d892
Your Name <you@example.com>
2024-02-18 00:49:55 +0000

    Experiment with disabling the adapter and the controlnet

--- infer.py
+++ infer.py
@@ -36,59 +36,79 @@ def resize_img(input_image, max_side=1280, min_side=1024, size=None,
 
 #%%
 
-if __name__ == "__main__":
+# Load face encoder
+app = FaceAnalysis(name='antelopev2', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640, 640))
 
-    # Load face encoder
-    app = FaceAnalysis(name='antelopev2', root='./', providers=['CUDAExecutionProvider', 'CPUExecutionProvider'])
-    app.prepare(ctx_id=0, det_size=(640, 640))
+face_image = load_image("./examples/yann-lecun_resize.jpg")
+face_image = resize_img(face_image)
 
-    face_image = load_image("./examples/yann-lecun_resize.jpg")
-    face_image = resize_img(face_image)
-
-    face_info = app.get(cv2.cvtColor(np.array(face_image), cv2.COLOR_RGB2BGR))
-    face_info = sorted(face_info, key=lambda x:(x['bbox'][2]-x['bbox'][0])*x['bbox'][3]-x['bbox'][1])[-1] # only use the maximum face
-    face_emb = face_info['embedding']
-    face_kps = pipeline_stable_diffusion_xl_instantid.draw_kps(face_image, face_info['kps'])
+face_info = app.get(cv2.cvtColor(np.array(face_image), cv2.COLOR_RGB2BGR))
+face_info = sorted(face_info, key=lambda x:(x['bbox'][2]-x['bbox'][0])*x['bbox'][3]-x['bbox'][1])[-1] # only use the maximum face
+face_emb = face_info['embedding']
+face_kps = pipeline_stable_diffusion_xl_instantid.draw_kps(face_image, face_info['kps'])
 
 #%%
-if __name__ == "__main__":
 
-    # Path to InstantID models
-    face_adapter = f'./checkpoints/ip-adapter.bin'
-    controlnet_path = f'./checkpoints/ControlNetModel'
+# Path to InstantID models
+face_adapter = f'./checkpoints/ip-adapter.bin'
+controlnet_path = f'./checkpoints/ControlNetModel'
 
-    # Load pipeline
-    controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16)
+# Load pipeline
+controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16)
 
-    base_model_path = 'stabilityai/stable-diffusion-xl-base-1.0'
+base_model_path = 'stabilityai/stable-diffusion-xl-base-1.0'
 
+do_load_controlnet = True
+
+if do_load_controlnet:
     pipe = pipeline_stable_diffusion_xl_instantid.StableDiffusionXLInstantIDPipeline.from_pretrained(
         base_model_path,
         controlnet=controlnet,
         torch_dtype=torch.float16,
     )
-    pipe.cuda()
-    pipe.load_ip_adapter_instantid(face_adapter)
+else:
+    pipe = pipeline_stable_diffusion_xl_instantid.StableDiffusionXLInstantIDPipeline.from_pretrained(
+        base_model_path,
+        torch_dtype=torch.float16,
+    )
 
 #%%
 
-if __name__ == "__main__":
-    # Infer setting
-    prompt = "analog film photo of a man. faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage, masterpiece, best quality"
-    n_prompt = "(lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured (lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch,deformed, mutated, cross-eyed, ugly, disfigured"
+pipe.cuda()
 
-    image = pipe(
-        prompt=prompt,
-        negative_prompt=n_prompt,
-        image_embeds=face_emb,
-        image=face_kps,
-        controlnet_conditioning_scale=0.8,
-        ip_adapter_scale=0.8,
-        num_inference_steps=30,
-        guidance_scale=5,
-    ).images[0]
+#%%
 
-    image.save('result.jpg')
+do_load_adapter = False
+pipe.load_ip_adapter_instantid(face_adapter, do_load_adapter=do_load_adapter)
+
+#%%
+
+ip_adapter_scale = 0.8
+controlnet_conditioning_scale = 0.8
+num_inference_steps = 50
+seed = 0
+# Infer setting
+# prompt = "analog film photo of a man. faded film, desaturated, 35mm photo, grainy, vignette, vintage, Kodachrome, Lomography, stained, highly detailed, found footage, masterpiece, best quality"
+# n_prompt = "(lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch, deformed, mutated, cross-eyed, ugly, disfigured (lowres, low quality, worst quality:1.2), (text:1.2), watermark, painting, drawing, illustration, glitch,deformed, mutated, cross-eyed, ugly, disfigured"
+prompt = "film noir style, ink sketch|vector, male man, highly detailed, sharp focus, ultra sharpness, monochrome, high contrast, dramatic shadows, 1940s style, mysterious, cinematic"
+negative_prompt = "ugly, deformed, noisy, blurry, low contrast, realism, photorealistic, vibrant, colorful"
+
+generator = torch.Generator(device="cuda").manual_seed(seed)
+
+image = pipe(
+    prompt=prompt,
+    negative_prompt=negative_prompt,
+    image_embeds=face_emb,
+    image=face_kps,
+    controlnet_conditioning_scale=controlnet_conditioning_scale,
+    ip_adapter_scale=ip_adapter_scale,
+    num_inference_steps=num_inference_steps,
+    guidance_scale=5,
+    generator=generator
+).images[0]
+
+image.save(f'result-{seed=}-{num_inference_steps=}-{do_load_adapter=}-{ip_adapter_scale=}-{controlnet_conditioning_scale=}.jpg')
 
 #%%
 
--- pipeline_stable_diffusion_xl_instantid.py
+++ pipeline_stable_diffusion_xl_instantid.py
@@ -155,9 +155,10 @@ class StableDiffusionXLInstantIDPipeline(StableDiffusionXLControlNetPipeline):
             else:
                 raise ValueError("xformers is not available. Make sure it is installed correctly")
     
-    def load_ip_adapter_instantid(self, model_ckpt, image_emb_dim=512, num_tokens=16, scale=0.5):     
+    def load_ip_adapter_instantid(self, model_ckpt, image_emb_dim=512, num_tokens=16, scale=0.5, do_load_adapter=True):
         self.set_image_proj_model(model_ckpt, image_emb_dim, num_tokens)
-        self.set_ip_adapter(model_ckpt, num_tokens, scale)
+        if do_load_adapter:
+            self.set_ip_adapter(model_ckpt, num_tokens, scale)
         
     def set_image_proj_model(self, model_ckpt, image_emb_dim=512, num_tokens=16):
         
